d60634d20d9b77c28c48c437da48772e7a97631e
Kaleb Striplin <kaleb.striplin@thermofisher.com>
2025-11-08 16:01:26 -0800

    Adds src code with initial project code. Adds macroquad for a game engine and UI. Adds game logic to draw a tree and handle quitting.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # MiniForestSimulator
 This is a Rust program for simulating a very small subset of old growth forest behaviors.
+
+
+## Run
+
+```rust
+cargo run
+```
+
+
+## Acknowledgments
+Used the following text for learning rust
+
+*Programming Rust: Fast, Safe Systems Development
+Jim Blandy and Jason Orendorff
+2nd edition, O'Reilly 2021*
+
+Using macroquad game engine
+referenced https://macroquad.rs/examples/ snake.rs as a starting point
+
+## License 
+
+See attached MIT License
---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use macroquad::prelude::*;
+
+const SQUARES: i16 = 64;
+
+type Point = (i16, i16);
+
+struct Tree {
+    position: Point,
+    _size: i16,
+}
+
+#[macroquad::main("Mini Forest Sim")]
+async fn main() {
+    let mut tree: Tree = Tree {
+        position: (32, 32),
+        _size: 1,
+    };
+
+    let mut tree_count: i32 = 1;
+    let mut end_sim: bool = false;
+
+    loop {
+        // this block updates the game state
+        if !end_sim && is_key_down(KeyCode::Q) {
+            end_sim = true;
+        }
+        // this block updates the screen state
+        if !end_sim {
+            clear_background(LIGHTGRAY);
+
+            let game_size: f32 = screen_width().min(screen_height());
+            let offset_x: f32 = (screen_width() - game_size) / 2. + 10.;
+            let offset_y: f32 = (screen_height() - game_size) / 2. + 10.;
+            let sq_size: f32 = (screen_height() - offset_y * 2.) / SQUARES as f32;
+
+            // draw background
+            draw_rectangle(offset_x, offset_y, game_size - 20., game_size - 20., WHITE);
+
+            // draw horizontal lines to form boxes
+            for i in 1..SQUARES {
+                draw_line(
+                    offset_x,
+                    offset_y + sq_size * i as f32,
+                    screen_width() - offset_x,
+                    offset_y + sq_size * i as f32,
+                    2.,
+                    LIGHTGRAY,
+                );
+            }
+
+            // draw vertical lines to form boxes
+            for i in 1..SQUARES {
+                draw_line(
+                    offset_x + sq_size * i as f32,
+                    offset_y,
+                    offset_x + sq_size * i as f32,
+                    screen_height() - offset_y,
+                    2.,
+                    LIGHTGRAY,
+                );
+            }
+
+            // draw tree as a green square
+            draw_rectangle(
+                offset_x + tree.position.0 as f32 * sq_size,
+                offset_y + tree.position.1 as f32 * sq_size,
+                sq_size,
+                sq_size,
+                DARKGREEN,
+            );
+
+            // draw instructions
+            draw_text(
+                format!("Tree count {tree_count} Press Q to quit").as_str(),
+                10.,
+                20.,
+                20.,
+                DARKGRAY,
+            );
+        // this block draws game over
+        } else {
+            // leave the screen
+            clear_background(WHITE);
+            let text = "Simulation ended. Press [enter] to play again.";
+            let font_size = 30.;
+            let text_size: TextDimensions = measure_text(text, None, font_size as _, 1.0);
+
+            draw_text(
+                text,
+                screen_width() / 2. - text_size.width / 2.,
+                screen_height() / 2. + text_size.height / 2.,
+                font_size,
+                DARKGRAY,
+            );
+
+            if is_key_down(KeyCode::Enter) {
+                tree = Tree {
+                    position: (16, 16),
+                    _size: 1,
+                };
+                tree_count = 1;
+                end_sim = false;
+            }
+        }
+        next_frame().await;
+    }
+}
